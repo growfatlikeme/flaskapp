5d0495ab7ffad5fda8ee84510d125b6fda02ce8a
GrowFatLikeMe <freakishcrap@gmail.com>
2025-07-22 20:20:16 +0800

    Add AWS X-Ray integration and update requirements for improved monitoring

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+app_config = os.environ.get('MY_APP_CONFIG')
+db_password = os.environ.get('MY_DB_PASSWORD')
+service_name = os.environ.get('SERVICE_NAME')
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Growfatttttt!</p>"
+xray_recorder.configure(service=service_name)
+XRayMiddleware(app, xray_recorder)
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    return jsonify({
+        "message": "Hello from Growfat Flask!",
+        "app_config": app_config,
+        "db_password": db_password
+    })
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
